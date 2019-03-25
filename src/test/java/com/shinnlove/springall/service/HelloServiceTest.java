774b3dc5ee4ce9159beae419f3913f8e3c953e69
chensheng.zcs <chensheng.zcs@alipay.com>
2019-03-25 14:55:19 +0800

    提交dubbo服务

--- src/test/java/com/shinnlove/springall/service/HelloServiceTest.java
+++ src/test/java/com/shinnlove/springall/service/HelloServiceTest.java
@@ -4,16 +4,29 @@
  */
 package com.shinnlove.springall.service;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author shinnlove.jinsheng
  * @version $Id: HelloServiceTest.java, v 0.1 2019-03-25 14:49 shinnlove.jinsheng Exp $$
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:/META-INF/spring/applicationContext.xml",
+                                   "classpath:/META-INF/spring/dubbo-rpc.xml" })
 public class HelloServiceTest {
 
-    public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext();
+    @Test
+    public void test_rpcService() {
+        try {
+            TimeUnit.SECONDS.sleep(30);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
