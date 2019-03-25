94455ab076abc0557d41f3176bfe70dca4eee0cd
chensheng.zcs <chensheng.zcs@alipay.com>
2019-03-25 16:39:17 +0800

    调整dubbo服务。

--- dubbo-provider/src/main/java/com/shinnlove/springall/dubbo/provider/HelloServiceImpl.java
+++ dubbo-provider/src/main/java/com/shinnlove/springall/dubbo/provider/HelloServiceImpl.java
@@ -7,6 +7,8 @@ package com.shinnlove.springall.dubbo.provider;
 import com.shinnlove.springall.dubbo.api.HelloService;
 
 /**
+ * 服务提供者具体的服务实现。
+ *
  * @author shinnlove.jinsheng
  * @version $Id: HelloServiceImpl.java, v 0.1 2019-03-25 16:23 shinnlove.jinsheng Exp $$
  */
