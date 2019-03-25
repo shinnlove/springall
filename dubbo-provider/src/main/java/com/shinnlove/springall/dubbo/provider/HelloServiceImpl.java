2919101dd2f1cd9845241b1018e7c404998f31cd
chensheng.zcs <chensheng.zcs@alipay.com>
2019-03-25 16:37:00 +0800

    初始化dubbo项目多模块maven布局

---
+++ dubbo-provider/src/main/java/com/shinnlove/springall/dubbo/provider/HelloServiceImpl.java
@@ -0,0 +1,20 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2019 All Rights Reserved.
+ */
+package com.shinnlove.springall.dubbo.provider;
+
+import com.shinnlove.springall.dubbo.api.HelloService;
+
+/**
+ * @author shinnlove.jinsheng
+ * @version $Id: HelloServiceImpl.java, v 0.1 2019-03-25 16:23 shinnlove.jinsheng Exp $$
+ */
+public class HelloServiceImpl implements HelloService {
+
+    @Override
+    public String sayHello(String name) {
+        return "Hello, " + name + ".";
+    }
+
+}
