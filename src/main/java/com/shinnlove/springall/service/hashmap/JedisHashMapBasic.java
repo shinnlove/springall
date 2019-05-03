e3ea9c1653ca3c1fb56aab804891813cd36f320d
shinnlove <306086640@qq.com>
2019-05-03 20:15:53 +0800

    增加Redis的HashMap的使用

---
+++ src/main/java/com/shinnlove/springall/service/hashmap/JedisHashMapBasic.java
@@ -0,0 +1,15 @@
+package com.shinnlove.springall.service.hashmap;
+
+/**
+ * jedis的HashMap基础使用。
+ *
+ * @author shinnlove.jinsheng
+ * @version $Id: JedisStringBasic.java, v 0.1 2019-05-01 10:50 shinnlove.jinsheng Exp $$
+ */
+public class JedisHashMapBasic {
+
+    public static void main(String[] args) {
+
+    }
+
+}
