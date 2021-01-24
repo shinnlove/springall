01a998a6a54e7c25816083040a13e0d37c10c4f7
chensheng.zcs <chensheng.zcs@alipay.com>
2021-01-24 23:08:36 +0800

    Submit brief answers to add two string numbers.

---
+++ src/main/java/com/shinnlove/springall/algorithm/AddStringAnswers.java
@@ -0,0 +1,33 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2021 All Rights Reserved.
+ */
+package com.shinnlove.springall.algorithm;
+
+/**
+ * LeetCode 43 answers.
+ * 
+ * wiki: https://leetcode-cn.com/problems/multiply-strings/
+ * 
+ * @author Tony Zhao
+ * @version $Id: AddStringAnswers.java, v 0.1 2021-01-24 11:07 PM Tony Zhao Exp $$
+ */
+public class AddStringAnswers {
+
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1, j = num2.length() - 1, add = 0;
+        StringBuffer ans = new StringBuffer();
+        while (i >= 0 || j >= 0 || add != 0) {
+            int x = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int y = j >= 0 ? num2.charAt(j) - '0' : 0;
+            int result = x + y + add;
+            ans.append(result % 10);
+            add = result / 10;
+            i--;
+            j--;
+        }
+        ans.reverse();
+        return ans.toString();
+    }
+
+}
